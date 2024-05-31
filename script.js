62a502b19d66232d7e6606cd46f5d7a6790e35f2
MatthewPass24 <matthewpass24@gmail.com>
2024-05-30 21:17:14 -0400

    added card styling and fixed spacing issue

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ const teamMembers = [
     {
         rank: '1.',
         name: 'Superman',
-        age: 36,
         activelyPlaying: true,
-        position: 'Forward',
-        skills: ['Flight', 'Super Strength', 'Heat Vision'],
+        status: 'Immortal',
+        powers: ['Flight', 'Super Strength', 'Heat Vision'],
         strengths: 'Leadership and Integrity', 
         weaknesses: 'Kryptonite',
         biography: 'Superman was sent to Earth after his planet, Krypton, was destroyed. With his great powers, he seeks to protect the world and achieve justice.',
@@ -16,8 +15,8 @@ const teamMembers = [
     {
         rank: '2.',
         name: 'Thor',
-        position: 'Midfielder',
-        skills: ['Energy Manipulation', 'Mjolnir control', 'Durability'],
+        status: 'Mortal',
+        powers: ['Energy Manipulation', 'Mjolnir control', 'Durability'],
         strengths: 'Bravery', 
         weaknesses: 'Stubborness and recklessness',
         biography: 'Thor is the God of Thunder and is the son of Odin. He wields the magical hammer Mjolnir and protects innocents across the nine realms.',
@@ -26,8 +25,8 @@ const teamMembers = [
     {
         rank: '3.',
         name: 'Dr. Manhattan',
-        position: 'Defender',
-        skills: ['Telekinesis', 'Reality Warping', 'Omniscience'],
+        status: 'Immortal',
+        powers: ['Telekinesis', 'Reality Warping', 'Omniscience'],
         strengths: 'Mastery over physics', 
         weaknesses: 'Apathy',
         biography: 'Physicist Jon Osterman developed the power to manipulate matter after a lab accident. He was dubbed Doctor Manhattan because of his limitless power. ',
@@ -36,8 +35,8 @@ const teamMembers = [
     {
         rank: '4.',
         name: 'Hulk',
-        position: 'Defender',
-        skills: ['Super Strength', 'Invulnerability', 'Regeneration'],
+        status: 'Immortal',
+        powers: ['Super Strength', 'Invulnerability', 'Regeneration'],
         strengths: 'Ball control', 
         weaknesses: 'Mind Control',
         biography: 'The Hulk is the alter ego of Dr. Bruce Banner who emerged after exposure to gamma radiation. Banner transforms when he is angry or stressed. ',
@@ -46,8 +45,8 @@ const teamMembers = [
     {
         rank: '5.',
         name: 'Wonder Woman',
-        position: 'Defender',
-        skills: ['Agility/reflexes', 'Super Healing', 'Magic Weaponry'],
+        status: 'Mortal',
+        powers: ['Agility/reflexes', 'Super Healing', 'Magic Weaponry'],
         strengths: 'Wisdom and Honesty', 
         weaknesses: 'Biological Clock',
         biography: 'Diana Prince is a highly trained pacifist and Amazon warrior-princess. She leads a disciplined but peaceful life with her fierce tribe of elite fighters.',
@@ -56,8 +55,8 @@ const teamMembers = [
     {
         rank: '6.',
         name: 'Doctor Strange',
-        position: 'Defender',
-        skills: ['Astral Projection', 'Time Travel', 'Spellcasting'],
+        status: 'Mortal',
+        powers: ['Astral Projection', 'Time Travel', 'Spellcasting'],
         strengths: 'Intelligence and Decisiveness', 
         weaknesses: 'Arrogance',
         biography: 'Doctor Stephen Strange was a talented neurosurgeon who was gravely injured after a car accident. Afterwards he must learn the secrets of mysticism and alternate dimensions.',
@@ -66,8 +65,8 @@ const teamMembers = [
     {
         rank: '7.',
         name: 'Scarlet Witch',
-        position: 'Defender',
-        skills: ['Chaos Magic', 'Telepathy', 'Transmutation'],
+        status: 'Mortal',
+        powers: ['Chaos Magic', 'Telepathy', 'Transmutation'],
         strengths: 'Mastery of Magic', 
         weaknesses: 'Inconsistent Control of Body',
         biography: 'The Scarlet Witch is  a mutant and born with superhuman abilities and traits. She was influenced by the High Evolutionary experimenting on her as a child.',
@@ -76,8 +75,8 @@ const teamMembers = [
     {
         rank: '8.',
         name: 'Sentry',
-        position: 'Defender',
-        skills: ['Immortality', 'Biokinesis', 'Telepathy'],
+        status: 'Immortal',
+        powers: ['Immortality', 'Biokinesis', 'Telepathy'],
         strengths: 'Heightened Senses', 
         weaknesses: 'The Void',
         biography: 'Robert Reynolds became the Sentry thanks to the Golden Sentry Serum. This serum changed his molecules and was a more advanced version of the super soldier serum.',
@@ -86,8 +85,8 @@ const teamMembers = [
     {
         rank: '9.',
         name: 'Shazam',
-        position: 'Defender',
-        skills: ['Power of Zeus', 'Transformation', 'Strength of Hercules'],
+        status: 'Immortal',
+        powers: ['Power of Zeus', 'Transformation', 'Strength of Hercules'],
         strengths: 'Purity of Heart and Courage', 
         weaknesses: 'Heading Ability',
         biography: 'Billy Batson is a young boy who transforms into the superhero by speaking "Shazam!" His powers come from six mythological figures, and he must stay pure.',
@@ -96,8 +95,8 @@ const teamMembers = [
     {
         rank: '10.',
         name: 'The Flash',
-        position: 'Defender',
-        skills: ['Super Speed', 'Time Travel', 'Speed Thinking'],
+        status: 'Mortal',
+        powers: ['Super Speed', 'Time Travel', 'Speed Thinking'],
         strengths: 'Intuitive and Adaptable', 
         weaknesses: 'Extreme Temperatures',
         biography: 'Barry Allen gained super speed when lightning hit him and was doused in chemicals. He took the name "Flash" and became one of the founding members of the Justice League.',
@@ -106,8 +105,8 @@ const teamMembers = [
     {
         rank: '11.',
         name: 'Frank Richards',
-        position: 'Defender',
-        skills: ['Psionic Abilities', 'Reality Manipulation', 'Pocket Universes'],
+        status: 'Mortal',
+        powers: ['Psionic Abilities', 'Reality Manipulation', 'Pocket Universes'],
         strengths: 'Intelligence', 
         weaknesses: 'Instability',
         biography: 'Franklin Richards is the son of Reed Richards and Sue Storm. He is an Omega-level mutant with reality-warping abilities, making him one of the most powerful beings.',
@@ -116,8 +115,8 @@ const teamMembers = [
     {
         rank: '12.',
         name: 'Supergirl',
-        position: 'Defender',
-        skills: ['Super Strength', 'Super Speed', 'Solar Energy Absorption'],
+        status: 'Immortal',
+        powers: ['Super Strength', 'Super Speed', 'Solar Energy Absorption'],
         strengths: 'Combat Trained', 
         weaknesses: 'Heading Ability',
         biography: 'Kara Zor-El is the cousin of Superman and shares similar powers. She was sent to Earth to protect her cousin, but her spacecraft was knocked off course and arrived much later.  ',
@@ -133,35 +132,34 @@ function generateTeamCards (){
         const card = document.createElement('div')
         card.classList.add('col-md-4')
 
-        //styling card based on position:
+        //styling card based on status:
         let backgroundColor = ''
 
-        switch(member.position.toLowerCase()){
-            case 'forward':
+        switch(member.status.toLowerCase()){
+            case 'immortal':
                 backgroundColor = '#758fb6'// orange for forward
                 break
-            case 'midfielder':
-                backgroundColor = '#758fb6'// orange for forward
+            case 'mortal':
+                backgroundColor = '#96e1e6'// orange for forward
                 break    
-            case 'defender':
-                backgroundColor = '#758fb6'// orange for forward
-                break              
+  
         }   
 
 
 
         //Create a list of skills with the <li> tag
-        const skillsList = member.skills.map(skill => `<li> ${skill} </li>`).join('')
+        const powersList = member.powers.map(power => `<li> ${power} </li>`).join('')
         card.innerHTML = `
-            <div class = "card h-100 mb-3" style= "margin-top: 2em;">
+
+        </div><div class = "card h-100">
                 <div class = "card-header" style="background-color: ${backgroundColor}">             
                 <h3><b>${member.rank}</b> ${member.name} </h3></div>
                 <div class = "card-body">
                 <img style="width: 200px" src=${member.image} style="justify-content-center">
-                    <p><strong>Position:</strong> ${member.position}</p>
-                    <p><strong>Skills:</strong>
+                    <p><strong>Status:</strong> ${member.status}</p>
+                    <p><strong>Powers:</strong>
                         <ul>
-                        ${skillsList}
+                        ${powersList}
                         </ul>
                     <p><strong>Strengths:</strong> ${member.strengths}</p>
                     <p><strong>Weaknesses:</strong> ${member.weaknesses}</p>
